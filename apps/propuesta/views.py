fde2d07d50334c0706e23ee9818fceaf374b50be
ArmoaD <pythond07@gmail.com>
2018-12-17 23:02:01 -0300

    continuacion Propuesta

--- apps/propuesta/views.py
+++ apps/propuesta/views.py
@@ -5,7 +5,7 @@ from apps.propuesta.forms import Propuesta_Form, ChequeFormSet
 def Registrar_Propuesta(request):
     Template_Name = "propuesta/propuesta.html"
     form = Propuesta_Form()
-    form_cheque = ChequeFormSet()
+    form_cheque = ChequeFormSet(prefix='cheque')
     if request.method == 'POST':
         form = Propuesta_Form(request.POST)
         if form.is_valid():
